bcc803c752dd37631459984a1c47ed666b5c6361
MahdiAbabneh <116374395+MahdiAbabneh@users.noreply.github.com>
2023-10-13 03:12:02 +0300

    complete app last part

--- kides zoo/mainView.swift
+++ kides zoo/mainView.swift
@@ -10,26 +10,13 @@ import SwiftUI
 struct mainView: View {
     
     @AppStorage("Paging")var paging:Int=0
-    @State var animalArrayImageName : [String] = ["cat","fox","lion","rakoon","shark","tiger","cat","fox","lion","rakoon","shark","tiger"]
-    var gridLayoutColoum : [GridItem]=[
-        GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]
+  
     
     var body: some View {
         VStack{
             mainViewTabBar().padding(.bottom,30)
-            LazyVGrid(columns:gridLayoutColoum){
-                ForEach(animalArrayImageName,id: \.self){
-                    item in
-                    Image(item)
-                        .resizable()
-                        .scaledToFill()
-                        .padding()
-                        .background(Color("AccentColor"))
-                        .frame(width: 90, height: 90,alignment: .center)
-                        .cornerRadius(50)
-                }
-                
-            }
+            MatchedItemView()
+
             Spacer()
 
         }
